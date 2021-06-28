50577e11f4924a3b292061bdd66a2de33697e842
Danniedan <86239323+Danniedan@users.noreply.github.com>
2021-06-28 23:51:25 +0800

    adding gSmart-CPU

---
+++ gSmart-CPU/gsmart_l5-l8.c
@@ -0,0 +1,348 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+const int num_con = 2;
+const int num_pre_l2 = 1;
+
+
+int main(int argc, char** argv)
+{
+  struct timeval startt;
+  struct timeval endt;
+  unsigned long timer;
+  
+  FILE *fr, *fc;
+  int M;
+	int N;
+	long nnz, i, j, it, nzr, nzc;
+  
+
+	char * filename1="./data/wat100r_l5.txt";
+	char * filename2="./data/wat100c_l5.txt";
+
+    if ((fr = fopen(filename1, "r")) == NULL) 
+            exit(1);
+	
+	fscanf(fr, "%d	%d	%ld", &(M), &(N), &(nnz));
+	int num_row;
+	num_row=M;
+	printf("num_row=%d, nnz=%ld\n", num_row, nnz);
+	
+    /* reseve memory for matrices */
+
+	int *subs_r = (int *) malloc(nnz * sizeof(int));
+	int* obs_r = (int *) malloc(nnz * sizeof(int));
+	int* pres_r = (int *) malloc(nnz * sizeof(int));
+	
+  
+    /* read raw data for LSpM_CSR */
+  
+	int direc_consis[2]={0, 2};
+	int direc_op[1]={1};
+	
+	long g;
+	int sub, pre, ob;
+	g=0;
+  
+  for (i=0; i<nnz; i++)
+  {
+		fscanf(fr, "%d	%d	%d", &(sub), &(pre), &(ob));
+		for(it=0; it<4; it++)
+		{
+			if(pre==direc_consis[it])
+			{
+				subs_r[g]=sub;
+				pres_r[g]=pre;
+				obs_r[g]=ob;
+				g++;
+				break;
+			}
+		}
+  }
+	nzr=g;
+
+	if (fr !=stdin) fclose(fr);
+	
+	
+    /* LSpM_CSR storing */
+
+	int num_r;
+	int *num_nonzeros = (int *) malloc(num_row * sizeof(int));
+	for(i=0; i<num_row; i++)
+		num_nonzeros[i]=0;
+	int* Mr = (int *) malloc((num_row+1) * sizeof(int));
+	
+
+	int sum=0;
+	for(i=0;i<nzr;i++)
+	{
+		num_nonzeros[subs_r[i]]++;
+	}
+	free(subs_r);
+
+	int k;
+	
+	g=0;
+	Mr[0]=0;
+	for(i=0; i<num_row; i++)
+	{
+		if(num_nonzeros[i]==0)
+			Mr[i+1]=g;
+		else
+		{	
+			g++;
+			Mr[i+1]=g;
+		}
+	}
+	num_r=g;
+  
+  
+	int* Ap_r = (int *) malloc((num_r+1) * sizeof(int));
+	int maxl_r=0;
+	Ap_r[0]=0;
+	sum=0;
+	g=0;
+
+	for(i=0; i<num_row; i++)
+	{
+		if(num_nonzeros[i]!=0)
+		{	
+			g++;
+			sum+=num_nonzeros[i];
+			Ap_r[g]=sum;
+			if(maxl_r < (Ap_r[g]-Ap_r[g-1]))
+			{
+				maxl_r=Ap_r[g]-Ap_r[g-1];	
+			}
+		}
+	}
+	free(num_nonzeros);
+	
+	
+	
+	
+	
+	if ((fc = fopen(filename2, "r")) == NULL) 
+            exit(1);
+	
+	fscanf(fc, "%d	%d	%ld", &(M), &(N), &(nnz));
+	
+	int num_column;
+	num_column=N;
+	printf("num_column=%d, nnz=%ld\n", num_column, nnz);
+	
+    /* reseve memory for matrices */
+
+	int *obs_c = (int *) malloc(nnz * sizeof(int));
+	int* subs_c = (int *) malloc(nnz * sizeof(int));
+	int* pres_c = (int *) malloc(nnz * sizeof(int));
+	
+  
+    /* read raw data for LSpM_CSC */
+	g=0;
+  for (i=0; i<nnz; i++)
+  {
+		fscanf(fc, "%d	%d	%d", &(sub), &(pre), &(ob));
+		for(it=0; it<2; it++)
+		{
+			if(pre==direc_op[it])
+			{
+				subs_c[g]=sub;
+				pres_c[g]=pre;
+				obs_c[g]=ob;
+				g++;
+				break;
+			}
+		}
+  }
+	nzc=g;
+	if (fc !=stdin) fclose(fc);
+	
+	
+    /* LSpM_CSC storing */
+  
+	int num_c;
+	num_nonzeros = (int *) malloc(num_column * sizeof(int));
+	for(i=0; i<num_column; i++)
+		num_nonzeros[i]=0;
+	int* Mc = (int *) malloc((num_column+1) * sizeof(int));
+	
+	for(i=0;i<nzc;i++)
+	{
+		num_nonzeros[obs_c[i]]++;
+	}
+	free(obs_c);
+
+
+	g=0;
+	Mc[0]=0;
+	for(i=0; i<num_column; i++)
+	{
+		if(num_nonzeros[i]==0)
+			Mc[i+1]=g;
+		else
+		{	
+			g++;
+			Mc[i+1]=g;
+		}
+	}
+	num_c=g;
+
+	int* Ap_c = (int *) malloc((num_c+1) * sizeof(int));
+	int maxl_c=0;
+	Ap_c[0]=0;
+	sum=0;
+	g=0;
+
+	for(i=0; i<num_column; i++)
+	{
+		if(num_nonzeros[i]!=0)
+		{	
+			g++;
+			sum+=num_nonzeros[i];
+			Ap_c[g]=sum;
+			if(maxl_c < (Ap_c[g]-Ap_c[g-1]))
+			{
+				maxl_c=Ap_c[g]-Ap_c[g-1];	
+			}
+		}
+	}
+	free(num_nonzeros);
+	
+
+
+	  /* light query evaluation */
+  
+	int *bind1_1 = (int *) malloc(maxl_c * sizeof(int));
+	for(i=0; i<maxl_c; i++)
+		bind1_1[i] = 0;
+	int *bind1_2 = (int *) malloc(num_row * sizeof(int));
+	for(i=0; i<num_row; i++)
+		bind1_2[i] = 0;
+	int *Bp1 = (int *) malloc(num_con * sizeof(int));
+	for(i=0; i<(num_con); i++)
+		Bp1[i]=0;
+	
+
+	//constants 52, 16, 184, 
+	int con[num_con] = {274, 169};
+	int con_pre[num_con] = {1, 0};
+	int flag;
+	if( (Mc[con[0]+1]-Mc[con[0]])==1 )
+	{
+		k=0;
+		for(i=Ap_c[Mc[con[0]]]; i<Ap_c[Mc[con[0]]+1]; i++)
+		{
+			if( pres_c[i]==con_pre[0] )
+			{
+				bind1_1[k] = subs_c[i];
+				k++;
+			}
+		}
+		Bp1[0]=k;
+		
+	}
+	
+	if( (Mr[con[1]+1]-Mr[con[1]])==1 )
+	{
+		k=0;
+		for(i=Ap_r[Mr[con[1]]]; i<Ap_r[Mr[con[1]]+1]; i++)
+		{
+			if( pres_r[i]==con_pre[1] )
+			{
+				bind1_2[obs_r[i]] = 1;
+				k++;
+			}
+		}
+		Bp1[1]=k;
+		
+	}
+	
+	
+  
+    /* main computation */
+	
+	int *bind2 = (int *) malloc(num_pre_l2*Bp1[0]*maxl_r * sizeof(int));
+	int edge_pre_l2[num_pre_l2]={2};
+	int ind_l2[num_pre_l2];
+	for(i=0; i<num_pre_l2; i++)
+		ind_l2[i]=0;
+	for(i=0; i<num_pre_l2*Bp1[0]*maxl_r; i++)
+		bind2[i]=-1;
+		
+		
+	int p, q;
+  
+	//evaluate 0th-level query edges
+	for(i=0; i<Bp1[0]; i++)
+	{
+		for(it=0; it<num_pre_l2; it++)
+			ind_l2[it] = 0;
+		for(g=0; g<num_pre_l2; g++)
+		{
+			flag=0;
+			if( (Mr[bind1_1[i]+1]-Mr[bind1_1[i]])==1 )
+			{
+				for(j=Ap_r[Mr[bind1_1[i]]]; j<Ap_r[Mr[bind1_1[i]]+1]; j++)
+				{
+					if( (pres_r[j]==edge_pre_l2[g]) )
+					{
+						bind2[g*Bp1[0]*maxl_r+i*maxl_r+ind_l2[g]] = obs_r[j];
+						ind_l2[g]++;
+						flag=1;
+					}
+				}
+				if(flag==0)
+				{
+					bind1_1[i] = -1;
+					for(it=0; it<g; it++)
+						ind_l2[it] = 0;
+					break;
+				}
+				
+			}
+			else
+			{
+				bind1_1[i] = -1;
+				break;
+			}
+		}
+	}
+	
+  
+    /* local tree-pruning */
+  
+	int *bind2_1=(int *) malloc(num_row * sizeof(int));
+	p=0;
+	for(i=0; i<Bp1[0]; i++)
+	{
+		if( (bind1_1[i]!=-1))
+		{
+			for(j=0; j<maxl_r; j++)
+			{
+				if( (bind2[i*maxl_r+j]!=-1) )
+				{
+					bind2_1[bind2[i*maxl_r+j]]=1;	
+				}
+			}
+		}
+	}
+	
+  
+	for(i=0; i<num_row; i++)
+	{
+		if( (bind2_1[i]==1)&&(bind1_2[i]==1) )
+			p++;
+	}
+
+	
+	return 0;
+}
+
